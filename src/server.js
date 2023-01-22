edffe30d1ec90c5ad3a9ff8114b207788098c6a9
Manelitu <emanuelwork1@gmail.com>
2023-01-22 09:26:57 -0300

    feat: create general methods POST and GET

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import http from 'node:http';
 
-const server = http.createServer(() => {
+const users = [];
 
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === "/users") {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === "/users") {
+    users.push({
+      id: 1,
+      name: 'Any name',
+      email: 'any_email@any.com'
+    })
+    return res.end('Create user');
+  }
+
+  return res.end('Hello world');
 });
 
 server.listen(3333);
