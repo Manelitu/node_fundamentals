2611d31210f5943c687bcbbfd0eb201984e66d15
Manelitu <emanuelwork1@gmail.com>
2023-01-22 10:11:10 -0300

    feat: create readable, transform and writable stream to study

---
+++ streams/fundamentals.js
@@ -0,0 +1,51 @@
+// Streams -> 
+
+// process.stdin
+//   .pipe(process.stdout);
+
+// process.stdin é o processo de entrada
+// process.stdout é o processo de saída 
+// pipe é o evento que leva o processo de entrada até a saida
+
+import {
+  Readable,
+  Writable,
+  Transform,
+  
+} from 'node:stream';
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buffer = Buffer.from(String(i));
+        this.push(buffer);
+      }
+    }, 1000);
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
+
