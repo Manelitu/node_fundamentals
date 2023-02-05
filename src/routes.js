9ce1e0d7a86db6439379f18f3574f4f55dd536d3
Manelitu <emanuelwork1@gmail.com>
2023-02-05 10:55:44 -0300

    feat: create delete method

--- src/routes.js
+++ src/routes.js
@@ -35,6 +35,12 @@ export const routes = [
   {
     method: 'DELETE',
     path: buildRoutePath('/users/:id'),
-    handler: (req, res) => res.end(),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete('users', id);
+
+      return res.writeHead(204).end();
+    },
   }
 ];
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 
 const server = http.createServer(async (req, res) => {
@@ -9,10 +10,14 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+    
+    req.params = { ... routeParams.groups};
+
     return route.handler(req, res);
   }
 
